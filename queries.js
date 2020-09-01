130760ca9b7f027e2211d6ef8aa3405e16d9cf81
Mihirsinh Parmar <mihirsinh.parmar.it@gmail.com>
2020-09-01 17:27:24 +0530

    added initial getter queries

--- queries.js
+++ queries.js
@@ -4,109 +4,215 @@ const knex = require('knex')({
         host: '127.0.0.1',
         user: 'postgres',
         password: '12345678',
-        database: 'test_social_app'
+        database: 'server_test_social_app'
     }
 });
 
-const getAllUser = async () => {
-    const response = await knex('users_table').select('*');
-    console.log(response);
-};
+const queryById = async (tableName, columnName,search) => {
+    try {
+        const data = await knex(tableName).select()
+            .where(columnName, '=', search)
+        if(data.length)
+            return {
+                flag: true,
+                data
+            };
+        else
+            return{
+                flag: false,
+                data: "No Entries Found"
+            }
+    }catch (e) {
+        console.log(e);
+        return {
+            flag: false,
+            data: e.detail
+        };
+    };
+}
 
-const registerUser = async() => {
-
-};
-
-const registerFamily = async() => {
-
-};
-
-const createFf = async() => {
-
-};
+const getAllUser = async () =>  await knex('users_table').select('*');
 
 const getRequests = async() => {
+    const connectionRequest = await knex('connection_request').select('*');
+    return connectionRequest;
 
 };
 
 const getGroupRequests = async() => {
-
+    const groupRequest = await knex('ff_group_request').select('*');
+    return groupRequest;
 };
 
-const getGroupMembers = async() =>{
+const getGroupMembers = async({search}) => await queryById('ff_group_members', 'fk_ff_group_id',search);
 
-};
+const getMajorFamilyMembers = async({search}) => await queryById('users_table', 'fk_family_id',search);
 
-const getMajorFamilyMembers = async() => {
+const getSiblings = async({search}) => await queryById('sibling_relation', 'fk_sibling1_id',search);
 
-};
+const getFollowers = async({search}) => await queryById('relations_follows', 'followee_id',search);
 
-const getSiblings = async() => {
+const getFollowing = async({search}) => await queryById('relations_follows', 'follower_id',search);
 
-};
+const getMajorFamilyAdmins = async({search}) => await queryById('admin_nodes_family_mva', 'fk_family_id',search);
 
-const getFollowers = async() => {
+const getGroupAdminNodes = async({search}) => await queryById('admin_nodes_ff_mva', 'fk_ff_group_id',search);
 
-};
-
-const getFollowing = async() => {
-
-};
-
-const getMajorFamilyAdmins = async() => [
-
-];
-
-const getGroupAdminNodes = async() => {
-
-};
-
-const getAllRegisteredFamily = async() => {
-
-};
-
-const getUserGroups = async() => {
-
-};
+const getUserGroups = async({search}) => await queryById('ff_group_members', 'fk_member_id',search);
 
 const getGroupMemberFamilyList = async() => {
-
+    try {
+        const data = await knex('ff_group_members').select('fk_member_family_id').distinct()
+            .where('fk_ff_group_id', '=', search)
+        if(data.length)
+            return {
+                flag: true,
+                data
+            };
+        else
+            return{
+                flag: false,
+                data: "No Entries Found"
+            }
+    }catch (e) {
+        console.log(e);
+        return {
+            flag: false,
+            data: e.detail
+        };
+    };
 };
 
-const getUserByName = async() => {
-
+const getUserByString = async({search}) => {
+    try{
+        const data = await knex('users_table').select()
+                    .where('user_email_id', '~*', '.*' + search + '.*')
+                    .orWhere('user_fullname', '~*', '.*' + search + '.*')
+                    .orWhere('username', '~*', '.*' + search + '.*')
+        if(data.length)
+            return {
+                flag: true,
+                data
+            };
+        else
+            return{
+                flag: false,
+                data: "No Entries Found"
+            }
+    }catch (e) {
+        console.log(e);
+        return {
+            flag: false,
+            data: e.detail
+        };
+    };
 };
 
-const getUserByEmail = async() => {
+const getUserById = async({search}) => await queryById('users_table', 'user_id',search);
 
+const getFamilyByName = async({search}) => {
+    try{
+        const data = await knex('family_table').select()
+            .where('family_name', '~*', '.*' + search + '.*');
+        if(data.length)
+            return {
+                flag: true,
+                data
+            };
+        else
+            return{
+                flag: false,
+                data: "No Entries Found"
+            }
+    }catch (e) {
+        console.log(e);
+        return {
+            flag: false,
+            data: e.detail
+        };
+    };
 };
 
-const getUserById = async() => {
+const getFamilyById = async({search}) => await queryById('family_table', 'family_id',search);
 
+const getFamilyByRootNode = async({search}) => await queryById('family_table', 'fk_root_node_id',search);
+
+const getGroupByName = async({search}) => {
+    try{
+        const data = await knex('ff_table').select()
+            .where('ff_name', '~*', '.*' + search + '.*');
+        if(data.length)
+            return {
+                flag: true,
+                data
+            };
+        else
+            return{
+                flag: false,
+                data: "No Entries Found"
+            }
+    }catch (e) {
+        console.log(e);
+        return {
+            flag: false,
+            data: e.detail
+        };
+    };
 };
 
-const getFamilyByName = async() => {
+const getGroupById = async({search}) => await queryById('ff_table', 'ff_group_id',search);
 
+const getGroupByCreatorId = async({search}) => await queryById('ff_table', 'fk_creator_id',search);
+
+const registerUser = (data) => async() => {
+    try{
+        const response = await knex('users_table').insert([data]);
+        console.log(response);
+        return {
+            flag: true,
+            data: "User Registered"
+        };
+    }catch (e) {
+        console.log(e.detail);
+        return {
+            flag: false,
+            data: "Couldn't Update"
+        };
+    }
 };
 
-const getFamilyById = async() => {
-
+const registerFamily = (data) => async() => {
+    try{
+        const response = await knex('family_table').insert([data]);
+        console.log(response);
+        return {
+            flag: true,
+            data: "Family Registered"
+        }
+    }catch (e) {
+        console.log(e.detail);
+        return{
+            flag: false,
+            data: "Couldn't Register a Family"
+        }
+    }
 };
 
-const getFamilyByRootNode = async() => {
-
-};
-
-const getGroupByName = async() => {
-
-};
-
-const getGroupById = async() => {
-
-};
-
-const getGroupByCreatorId = async() => {
-
+const createFf = (data) => async() => {
+    try{
+        const response = await knex('ff_table').insert([data]);
+        console.log(response);
+        return {
+            flag: true,
+            data: "Family Friends Group Registered"
+        }
+    }catch (e) {
+        console.log(e.detail);
+        return{
+            flag: false,
+            data: "Couldn't Register a Family Friends Group"
+        }
+    }
 };
 
 const promoteUserToAdminForFamily = async() => {
@@ -137,4 +243,29 @@ const sendRequest = async() =>{
 
 };
 
-module.exports = {getAllUser};
+module.exports = {
+    getAllUser,
+    registerUser,
+    registerFamily,
+    createFf,
+    getRequests,
+    getGroupRequests,
+    getUserByString,
+    getUserById,
+    getFamilyByName,
+    getFamilyById,
+    getFamilyByRootNode,
+    getGroupByName,
+    getGroupById,
+    getGroupByCreatorId,
+    getGroupMembers,
+    getMajorFamilyMembers,
+    getSiblings,
+    getFollowers,
+    getFollowing,
+    getMajorFamilyAdmins,
+    getGroupAdminNodes,
+    getUserGroups,
+    getGroupMemberFamilyList
+
+};
--- server.js
+++ server.js
@@ -8,6 +8,85 @@ const app = express();
 app.use(bp.json());
 app.use(cors());
 
+//-----------------------------------------------------------------------//
+//--------------------------- Getter Requests ---------------------------//
+//-----------------------------------------------------------------------//
+
+app.get('/getAllUser',async (req,res) => res.json(await query.getAllUser()));
+
+app.get('/getRequest', async (req,res) => res.json(await query.getRequests()));
+
+app.get('/getGroupRequest', async (req,res) => res.json(await query.getGroupRequests()));
+
+app.get('/getUserByString', async (req,res) => res.json(await query.getUserByString(req.body)));
+
+app.get('/getUserById', async (req,res) => res.json(await query.getUserById(req.body)));
+
+app.get('/getFamilyByName', async (req,res) => res.json(await query.getFamilyByName(req.body)));
+
+app.get('/getFamilyById', async (req,res) => res.json(await query.getFamilyById(req.body)));
+
+app.get('/getFamilyByRootNode', async (req,res) => res.json(await query.getFamilyByRootNode(req.body)));
+
+app.get('/getGroupByName', async (req,res) => res.json(await query.getGroupByName(req.body)));
+
+app.get('/getGroupById', async (req,res) => res.json(await query.getGroupById(req.body)));
+
+app.get('/getGroupByCreatorId', async (req,res) => res.json(await query.getGroupByCreatorId(req.body)));
+
+app.get('/getGroupMembers', async (req,res) => res.json(await query.getGroupMembers(req.body)));
+
+app.get('/getMajorFamilyMembers', async (req,res) => res.json(await query.getMajorFamilyMembers(req.body)));
+
+app.get('/getSiblings', async (req,res) => res.json(await query.getSiblings(req.body)));
+
+app.get('/getFollowers', async (req,res) => res.json(await query.getFollowers(req.body)));
+
+app.get('/getFollowing', async (req,res) => res.json(await query.getFollowing(req.body)));
+
+app.get('/getMajorFamilyAdmins', async (req,res) => res.json(await query.getMajorFamilyAdmins(req.body)));
+
+app.get('/getGroupAdminNodes', async (req,res) => res.json(await query.getGroupAdminNodes(req.body)));
+
+app.get('/getUserGroups', async (req,res) => res.json(await query.getUserGroups(req.body)));
+
+app.get('/getGroupMemberFamilyList', async (req,res) => res.json(await query.getGroupMemberFamilyList(req.body)));
+
+//---------------------------------------------------------------------//
+//--------------------------- Post Requests ---------------------------//
+//---------------------------------------------------------------------//
+
+app.post('/registerUser', async (req,res)=> {
+    const data = {created_on: new Date(), ...req.body}
+    const response = await query.registerUser(data)();
+    if(response.flag)
+        res.json("Welcome to Scoial Network");
+    else
+        res.json("Ooops! Try Again.");
+});
+
+app.post('/registerFamily', async (req,res)=> {
+    const data = { ...req.body};
+    const response = await query.registerFamily(data)();
+    if(response.flag)
+        res.json("Welcome to Scoial Network");
+    else
+        res.json("Ooops! Try Again.");
+});
+
+app.post('/CreateFf', async (req,res)=>{
+    const data = { created_on: new Date() ,...req.body};
+    const response = await query.createFf(data)();
+    if(response.flag)
+        res.json("Welcome to Social Network");
+    else
+        res.json("Ooops! Try Again.");
+});
+
+//-------------------------------------------------------------------------//
+//--------------------------- Listening at Port ---------------------------//
+//-------------------------------------------------------------------------//
+
 app.listen(3000,()=>
     console.log("Listening at Port 3000")
 );
